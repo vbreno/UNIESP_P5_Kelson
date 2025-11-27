1c9c0a8d78b79d3192dc8e31d364d7a7dfd7a36b
vbreno <vbrenodeveloper@gmail.com>
2025-11-26 23:52:33 -0300

    Registro da aula02 com abordagem em  estilização na linguagem Javascript

---
+++ react-native/aula02/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LayoutResponsivo from './src/components/LayoutResponsivo'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }} >
+        <LayoutResponsivo/>
+    </View>
+  )
+}
+
+export default App
